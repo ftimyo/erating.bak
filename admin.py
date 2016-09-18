cfa58e0e073527ec8f181bef2773c63c15b8c89d
tim <tim@dhcp-ccc-407.wifi-visitor.cit.cornell.edu>
2016-09-18 01:31:52 -0400

    add merchant catagory

--- admin.py
+++ admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import GreenMerchants
+from .models import GreenMerchants, GreenMerchantCat
 
 # Register your models here.
 @admin.register(GreenMerchants)
 class GreenMerchantsAdmin(admin.ModelAdmin):
     fields = ('mid','mname','mcat','mreward')
     list_display = ('mname','mreward')
+
+@admin.register(GreenMerchantCat)
+class GreenMerchantsCatAdmin(admin.ModelAdmin):
+    fields = ('catn',)
+    list_display = ('catn',)
--- models.py
+++ models.py
@@ -4,10 +4,16 @@ from django.contrib.auth.models import User
 class Bank(models.Model):
     uid = models.ForeignKey(User)
     cid = models.CharField('CustomerID',max_length=50)
+class GreenMerchantCat(models.Model):
+    catn = models.CharField('CatagoryName',max_length=50)
+    def __str__(self):
+        return self.catn
+    class Meta:
+        ordering = ('catn',)
 class GreenMerchants(models.Model):
     mid = models.CharField('MerchantID',max_length=50)
     mname = models.CharField('MerchantName',max_length=100)
-    mcat = models.TextField('MerchantCatagories')
+    mcat = models.ForeignKey(GreenMerchantCat)
     mreward = models.FloatField("Reward")
 
     def __str__(self):
